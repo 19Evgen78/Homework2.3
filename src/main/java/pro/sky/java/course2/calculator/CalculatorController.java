fd44445b57e1f5b4896459223c0cf0d462dd4bdd
19kuz <19kuzminykh78@mail.ru>
2023-09-30 16:41:26 +0700

    Домашку 2.3 исправил. Проверьте.

--- src/main/java/pro/sky/java/course2/calculator/CalculatorController.java
+++ src/main/java/pro/sky/java/course2/calculator/CalculatorController.java
@@ -7,13 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CalculatorController {
     private final CalculatorService calculatorService;
-    private int num1;
-    private int num2;
 
     public CalculatorController(CalculatorService calculatorService) {
         this.calculatorService = calculatorService;
-        this.num1 = num1;
-        this.num2 = num2;
     }
 
     @GetMapping(path = "/calculator")
@@ -22,19 +18,19 @@ public class CalculatorController {
     }
 
     @GetMapping(path = "/calculator/plus")
-    public int plusCalculator(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
-        return calculatorService.plusCalculator(num1 , num2);
+    public String plusCalculator(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
+        return num1 + " + " + num2 + " = " + (num1 + num2);
     }
     @GetMapping(path = "/calculator/minus")
-    public int minusCalculator(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
-        return calculatorService.minusCalculator(num1 , num2);
+    public String minusCalculator(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
+        return num1 + " - " + num2 + " = " + (num1 - num2);
     }
     @GetMapping(path = "/calculator/multiply")
-    public int multiplyCalculator(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
-        return calculatorService.multiplyCalculator(num1 , num2);
+    public String multiplyCalculator(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
+        return num1 + " * " + num2 + " = " + (num1 * num2);
     }
     @GetMapping(path = "/calculator/divide")
     public String divideCalculator(@RequestParam("num1") int num1, @RequestParam("num2") int num2) {
-        return calculatorService.divideCalculator(num1 , num2);
+        return num1 + " / " + num2 + " = " + (num1 / num2);
     }
 }
